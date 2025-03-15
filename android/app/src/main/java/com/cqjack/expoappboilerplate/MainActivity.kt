ad59e48cd2d72e54c0fe545f3e0c36518e64e185
Donald Lee <donaldlee50@gmail.com>
2025-03-15 16:36:14 -0500

    First commit, updating defaults

--- android/app/src/main/java/com/cqjack/expoappboilerplate/MainActivity.kt
+++ android/app/src/main/java/com/cqjack/expoappboilerplate/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.cqjack.expoappboilerplate
+package com.sleeprapp.app
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/cqjack/expoappboilerplate/MainApplication.kt
+++ android/app/src/main/java/com/cqjack/expoappboilerplate/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.cqjack.expoappboilerplate
+package com.sleeprapp.app
 
 import android.app.Application
 import android.content.res.Configuration
